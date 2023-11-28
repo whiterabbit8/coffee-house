d963ce0eed4ef5c2a927f55496909093b7da730d
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-11-28 23:27:04 +0300

    fix: disable interactivity on some components

--- coffee-house/vite.config.js
+++ coffee-house/vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite';
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        menu: resolve(__dirname, 'menu.html'),
+      },
+    },
+  },
   base: '/coffee-house/',
-});
+})
