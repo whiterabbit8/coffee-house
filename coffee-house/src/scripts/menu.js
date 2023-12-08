79e42a0f83981f3c4724acd2cb87fa0fbf4bc97e
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-12-08 13:46:54 +0300

    feat: implement burger menu work

--- coffee-house/src/scripts/menu.js
+++ coffee-house/src/scripts/menu.js
@@ -1,7 +1,10 @@
 const menuBtn = document.querySelector('.burger-menu-btn');
+const navMenu = document.querySelector('.header__nav');
 
 const toggleMenu = () => {
-  menuBtn.classList.toggle('active')
+  menuBtn.classList.toggle('active');
+  navMenu.classList.toggle('show');
+  document.body.classList.toggle('noscroll');
 }
 
 menuBtn.addEventListener('click', toggleMenu);
