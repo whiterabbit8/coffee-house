0833224f35107f0d71557277150320566e2dabcb
Natallia Ivanyuk <natashaivanyuk@gmail.com>
2023-12-08 14:02:20 +0300

    feat: close menu on lis item click

--- coffee-house/src/scripts/index.js
+++ coffee-house/src/scripts/index.js
@@ -1,4 +1,6 @@
 import { menuBtn, navItem, toggleMenu, closeMenu } from "./burgerMenu";
 
 menuBtn.addEventListener('click', toggleMenu);
-navItem.addEventListener('click', closeMenu);
+navItem.forEach((item) => {
+  item.addEventListener('click', closeMenu);
+})
